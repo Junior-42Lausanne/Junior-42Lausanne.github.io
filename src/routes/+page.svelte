db9ac5d5dbfb73b92799379fb85afb7583c9f537
Quentin Jungo <quentin.jungo@gmail.com>
2023-05-14 05:23:34 +0200

    adding skeleton UI and adding example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<h1 class="text-3xl font-bold underline">
-  Hello world!
-</h1>
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { AppBar, TableOfContents } from '@skeletonlabs/skeleton';
+	let value = 0;
+</script>
 
-<style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.gray.100);
-  }
-</style>
+<AppBar>
+	<svelte:fragment slot="lead">(icon)</svelte:fragment>
+	(title)
+	<svelte:fragment slot="trail">(actions)</svelte:fragment>
+</AppBar>
+
+<main id="h">
+	<h1 class="h1">
+		<span
+			class="text-9xl bg-gradient-to-br from-blue-500 to-cyan-300 bg-clip-text text-transparent box-decoration-clone"
+		>
+			We build amazing apps
+		</span>
+	</h1>
+	<section class="flex">
+		<section class="grow">
+			<h2 class="h2">Subject A</h2>
+			<h2 class="h2">Subject B</h2>
+		</section>
+		<section>
+			<TableOfContents target="#h" />
+		</section>
+	</section>
+</main>
