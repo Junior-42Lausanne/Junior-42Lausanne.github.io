e88180e5f56afefa521665020537afa5f885f9f3
Quentin Jungo <quentin.jungo@gmail.com>
2023-05-15 15:45:12 +0200

    adding a partner-card component

---
+++ src/lib/components/Partner.svelte
@@ -0,0 +1,18 @@
+<!-- a little card to display the logo of a partner -->
+
+<script lang="ts">
+	import { Avatar } from '@skeletonlabs/skeleton';
+
+	export let name: string;
+	export let logo: string;
+	export let link: string;
+</script>
+
+<div class="card flex flex-row p-8">
+  <Avatar width="w-12" rounded="rounded-full" initials={name} src={logo} />
+	<a href={link} class="flex flex-col items-left pl-4">
+
+		<h3 class="h3 pb-2">{name}</h3>
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+	</a>
+</div>
---
+++ src/routes/partners/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Partner from '$lib/components/Partner.svelte';
+</script>
+
+<div class="flex flex-col items-center justify-center">
+	<h1 class="h1">Partners</h1>
+	<p class="p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+
+	<!-- a div with a grid of partners -->
+	<div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+		<!-- a little card to display the logo of a partner -->
+		<Partner name={'je fribourg'} logo={'todo'} link={'todo'} />
+		<Partner name={'je fribourg'} logo={'todo'} link={'todo'} />
+		<Partner name={'je fribourg'} logo={'todo'} link={'todo'} />
+		<Partner name={'je fribourg'} logo={'todo'} link={'todo'} />
+		<Partner name={'je fribourg'} logo={'todo'} link={'todo'} />
+		<Partner name={'je fribourg'} logo={'todo'} link={'todo'} />
+	</div>
+</div>
