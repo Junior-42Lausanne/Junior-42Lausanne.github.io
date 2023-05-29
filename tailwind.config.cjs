afdc0e4ea05d336440106ecc84c98e3ef8c907ae
Quentin Jungo <quentin.jungo@gmail.com>
2023-05-29 18:05:02 +0200

    responsive WebService

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,9 +6,19 @@ module.exports = {
 		require('path').join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
 	],
 	theme: {
+		screens: {
+			'sm': '640px',  
+			'md': '768px',
+			'lg': '1024px',
+			'xl': '1280px',
+			'2xl': '1536px',
+		},
 		extend: {
 			screens: {
-				'md-max': { max: '1024px' }
+				'sm-max': { max: '639px' },
+				'md-max': { max: '1023px' },
+				'lg-max': { max: '1279px' },
+				'xl-max': { max: '1535px' },
 			},
 			spacing: {
 				'xscreen': '1024px', // le breakpoint entre md et lg = 1024px = la ou en passe en mode desktop
