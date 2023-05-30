88f0b8c110c0a80f381ad9d0534408d50d93f7e0
Quentin Jungo <quentin.jungo@gmail.com>
2023-05-30 10:52:22 +0200

    adding partners-logo

--- src/routes/partners/+page.svelte
+++ src/routes/partners/+page.svelte
@@ -11,18 +11,18 @@
 	const partners: PartnerT[] = [
 		{
 			name: '',
-			logo: 'https://i0.wp.com/bthcrhqa.preview.infomaniak.website/wp-content/uploads/2018/05/JEF-1.png?w=1080',
+			logo: 'partner-logos/JEF.webp',
 			href: 'https://jef.ch'
 		},
 		{
 			name: '',
-			logo: 'https://42lausanne.ch/wp-content/uploads/2021/01/42_logo.svg',
+			logo: 'partner-logos/42.svg',
 			href: 'https://42lausanne.ch/'
 		}
 	];
 </script>
 
-<Header name="Partenaires" description="Decouvrez nos qui nous font confiance" />
+<Header name="Partenaires" description="Decouvrez ceux qui nous font confiance" />
 
 <!-- <div class="flex flex-col items-center justify-center"> -->
 <div class={`grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2`}>
