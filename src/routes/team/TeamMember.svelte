c038f5b33c720fb45f7981d869b899374d05e04d
Quentin Jungo <43724937+JungoQuentin@users.noreply.github.com>
2023-05-30 14:37:48 +0200

    correct text truncate on member cards

--- src/routes/team/TeamMember.svelte
+++ src/routes/team/TeamMember.svelte
@@ -7,30 +7,32 @@
 	export let picture: string;
 	export let linkedin_link: string;
 	export let role: string;
-	// export let mail: string;
 
 	let isHovering = false;
-
 	const linkedin_logo =
 		'https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/768px-LinkedIn_logo_initials.png';
 </script>
 
-<div class="card card-hover flex flex-row flex-nowrap p-8">
+<div class="card card-hover flex flex-row flex-nowrap p-8" 
+on:mouseenter={() => (isHovering = true)}
+on:mouseleave={() => (isHovering = false)}
+
+>
 	<img class="w-32 rounded-full" alt={name} src={picture} />
-	<!-- <Avatar width="w-32" rounded="rounded-full" initials={name} src={picture} /> -->
 	<div
-		on:mouseenter={() => (isHovering = true)}
-		on:mouseleave={() => (isHovering = false)}
 		class:truncate={!isHovering}
-		class="mx-4 flex flex-1 flex-col truncate hover:text-clip"
+		class="mx-4 flex flex-1 flex-col  hover:text-clip"
 	>
-		<h2 class=" h2 truncate whitespace-nowrap pb-2">{name}</h2>
+		<h2 
+		class:whitespace-nowrap={!isHovering} 
+		class:truncate={!isHovering} 
+		
+		class=" h2 pb-2">{name}</h2>
 		<p>{role}</p>
 	</div>
 	<div class="flex flex-col justify-end gap-2">
 		<a rel="noopener noreferrer " target="”_blank”" href={linkedin_link}>
 			<Avatar width="w-8" rounded="rounded-full" initials="in" src={linkedin_logo} />
 		</a>
-		<!-- <a href={'mailto:' + mail}><Avatar width="w-8" rounded="rounded-xl" initials="@" /> </a> -->
 	</div>
 </div>
